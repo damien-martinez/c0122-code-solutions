a8214d2782b53d5401b3cd8ed7451ce10e8a48f1
Damien Martinez <mart.damien1@gmail.com>
2022-01-31 10:51:26 -0800

    Completed javascript-primitives-and-variables exercise.

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,19 @@
+var fullName = 'Damien Martinez';
+var isCool = false;
+var totalPets = 2;
+
+console.log('value of fullName ', fullName);
+console.log('value of isCool? ', isCool);
+console.log('value of Totalpets: ', totalPets);
+
+console.log('Type of for fullName variable', typeof (fullName));
+console.log('Type of for isCool variable', typeof (isCool));
+console.log('Type of for totalPets variable', typeof (totalPets));
+
+var unknown = null;
+console.log('unknown: ', unknown);
+console.log('Type of for unknown variable', typeof (unknown));
+
+var never;
+console.log('never: ', never);
+console.log('Type of for never variable', typeof (never));
