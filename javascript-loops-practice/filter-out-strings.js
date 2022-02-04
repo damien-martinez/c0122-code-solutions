b4ec133bf8c7d13612941f17890c3abe4b9539cf
Damien Martinez <mart.damien1@gmail.com>
2022-02-04 15:25:35 -0800

    Completed javascript-loops-practice exercise.

--- javascript-loops-practice/filter-out-strings.js
+++ javascript-loops-practice/filter-out-strings.js
@@ -4,7 +4,7 @@ function filterOutStrings(values) {
   var noStrings = [];
   for (var i = 0; i < values.length; i++) {
     // console.log('type of', typeof (values[i]));
-    if (typeof (values[i]) === 'number') {
+    if (typeof (values[i]) !== 'string') {
       noStrings.push(values[i]);
       // console.log('value', values[i]);
       // console.log('type', typeof (values[i]));
