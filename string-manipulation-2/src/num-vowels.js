1367ef2cbafff278acaecfb9d2481e8f398b3aab
Damien Martinez <mart.damien1@gmail.com>
2022-02-09 10:52:02 -0800

    In progress string-manipulation-2 exercise.

--- string-manipulation-2/src/num-vowels.js
+++ string-manipulation-2/src/num-vowels.js
@@ -1,1 +1,12 @@
 /* exported numVowels */
+function numVowels(string) {
+  var vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+  var count = 0;
+
+  for (var i = 0; i < string.length; i++) {
+    if (string[i] in vowels) {
+      count += 1;
+    }
+  }
+  return count;
+}
