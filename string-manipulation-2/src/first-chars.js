f083c4a008f18d0847cf0251a4140913809ad03c
Damien Martinez <mart.damien1@gmail.com>
2022-02-09 16:53:49 -0800

    In process string-manipulation-2 exercise.

--- string-manipulation-2/src/first-chars.js
+++ string-manipulation-2/src/first-chars.js
@@ -1,1 +1,27 @@
 /* exported firstChars */
+
+// function with two parameters that returns a new string that is the length of the length parameter
+
+// first creates a new string
+// checks if the length of the string is less than the length of the length parameter
+// if it is, it will return the inputted string
+// if not , of checks if it is an empty string.
+// if empty , will return an empty string .a
+// if neither condition works, it will run a for loop to interate through the string stops before it is incremented to the length parameter
+// it will add each character at the index
+
+// once broken out of for loop , will return new string
+
+function firstChars(length, string) {
+  var newString = '';
+  if (string.length < length) {
+    return string;
+  } else if (string.length < 1) {
+    return '';
+  } else {
+    for (var i = 0; i < length; i++) {
+      newString += string[i];
+    }
+  }
+  return newString;
+}
--- string-manipulation-2/src/swap-chars.js
+++ string-manipulation-2/src/swap-chars.js
@@ -1,1 +1,25 @@
 /* exported swapChars */
+
+// function takes in a string with 3 parameters, firstIndex, secondIndex, string. amd returns a new string where the character indexed from the first index parameter is swapped with the character indexed at the second index parameter
+
+// create a variable and assign it to the string at first index
+// create a variable and assign it to the string at second index
+// create a newstring empty variable
+
+function swapChars(firstIndex, secondIndex, string) {
+  var firstIndexChar = string[firstIndex];
+  var secondIndexChar = string[secondIndex];
+  var newString = '';
+  for (var i = 0; i < string.length; i++) {
+    if (i === firstIndex) {
+      newString += secondIndexChar;
+    } else if (i === secondIndex) {
+      newString += firstIndexChar;
+    } else {
+      newString += string[i];
+    }
+
+  }
+
+  return newString;
+}
