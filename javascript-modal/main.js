98becfb1e613d847ec6f8573816d29d0ebc30980
Damien Martinez <mart.damien1@gmail.com>
2022-02-12 23:30:03 -0800

    Completed javascript-modal exercise.

--- javascript-modal/main.js
+++ javascript-modal/main.js
@@ -1,5 +1,26 @@
-// var $openButton = document.querySelector('.open-button');
+var $openButton = document.querySelector('.open-button');
+var $modal = document.querySelector('.modal');
+var $overlay = document.querySelector('.overlay');
+var $noButton = document.querySelector('.no-button');
+var modalOpen = false;
 
-// function openModal(event) {
+function openModal(event) {
+  if (!modalOpen) {
+    $modal.className = 'row wrap position modal';
+    $overlay.className = 'overlay';
+    modalOpen = true;
+  }
 
-// }
+}
+
+function closeModal(event) {
+  if (modalOpen) {
+    $modal.className = 'row wrap position modal display-none';
+    $overlay.className = 'overlay display-none';
+    modalOpen = false;
+  }
+
+}
+
+$openButton.addEventListener('click', openModal);
+$noButton.addEventListener('click', closeModal);
