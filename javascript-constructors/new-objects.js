ad743758c422ae01db219951cdab0ba1f0877615
Damien Martinez <mart.damien1@gmail.com>
2022-02-22 11:28:03 -0800

    Completed javascript-constructors exercise.

---
+++ javascript-constructors/new-objects.js
@@ -0,0 +1,35 @@
+var anObject = new Object();
+
+var anArray = new Array();
+
+var aFunction = new Function();
+
+var aString = new String();
+
+var aNumber = new Number();
+
+var aBoolean = new Boolean();
+
+console.log('anObject', anObject);
+
+console.log('anArray', anArray);
+
+console.log('aFunction', aFunction);
+
+console.log('aString', aString);
+
+console.log('aNumber', aNumber);
+
+console.log('aBoolean', aBoolean);
+
+console.log('typeOf Object', typeof anObject);
+
+console.log('typeOf Array', typeof anArray);
+
+console.log('typeOf Function', typeof aFunction);
+
+console.log('typeOf String', typeof aString);
+
+console.log('typeOf Number', typeof aNumber);
+
+console.log('typeOf Boolean', typeof aBoolean);
