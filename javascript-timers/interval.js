c42dc8ed85919124a6a1ba610a124a2c10de8088
Damien Martinez <mart.damien1@gmail.com>
2022-02-23 15:50:49 -0800

    Fixed javascript-timers exercose.

--- javascript-timers/interval.js
+++ javascript-timers/interval.js
@@ -1,25 +1,17 @@
 var $display = document.querySelector('.countdown-display');
 var intervalID;
 
-function runFunction(number) {
-  intervalID = setInterval(changeDisplay, 1000);
-}
+intervalID = setInterval(changeDisplay, 1000);
 
-function changeDisplay(number) {
-  if ($display.textContent === '4') {
-    $display.textContent = '3';
+var counter = parseInt($display.textContent);
 
-  } else if ($display.textContent === '3') {
-    $display.textContent = '2';
-
-  } else if ($display.textContent === '2') {
-    $display.textContent = '1';
-
-  } else if ($display.textContent === '1') {
-    $display.textContent = '~Earth Beeeelooowww Us~';
+function changeDisplay() {
+  if (counter > 1) {
+    counter--;
+    $display.textContent = counter;
+  } else {
+    $display.textContent = 'Earth Beeeelooowww Us~';
     clearInterval(intervalID);
   }
 
 }
-
-runFunction($display.textContent);
