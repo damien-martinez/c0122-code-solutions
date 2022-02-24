9a638b81f9c1f2c5a146a9fa834edbeede0b5523
Damien Martinez <mart.damien1@gmail.com>
2022-02-23 23:16:21 -0800

    Still working on string-manipulation-3.

--- string-manipulation-3/src/reverse-words.js
+++ string-manipulation-3/src/reverse-words.js
@@ -1,1 +1,21 @@
 /* exported reverseWords */
+function reverseWords(string) {
+  var resverseString = '';
+  var subString = '';
+
+  for (var i = 0; i < string.length; i++) {
+    if (string[i] !== ' ') {
+      subString += string[i];
+    } else {
+      for (var j = subString.length - 1; j >= 0; j--) {
+        resverseString += subString[j];
+      }
+      resverseString += ' ';
+      subString = '';
+    }
+  }
+  for (j = subString.length - 1; j >= 0; j--) {
+    resverseString += subString[j];
+  }
+  return resverseString;
+}
