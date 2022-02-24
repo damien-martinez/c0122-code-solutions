43a54c2677f8c18a2965dc8e64605de0bb080e3d
Damien Martinez <mart.damien1@gmail.com>
2022-02-24 11:20:12 -0800

    string-manipulation-3 exercise.

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,1 +1,17 @@
 /* exported titleCase */
+
+// for (var i = 0; i < string.length; i++) {
+//   if (string[i] !== ' ') {
+//     subString += string[i];
+//   } else {
+//     for (var j = subString.length - 1; j >= 0; j--) {
+//       resverseString += subString[j];
+//     }
+//     resverseString += ' ';
+//     subString = '';
+//   }
+// }
+// for (j = subString.length - 1; j >= 0; j--) {
+//   resverseString += subString[j];
+// }
+// return resverseString;
