b6aa1e343ee8fddb4d0d58f91cfc31be6ad21764
Damien Martinez <mart.damien1@gmail.com>
2022-02-24 10:42:09 -0800

    Completed coding challange.

---
+++ main.js
@@ -0,0 +1,29 @@
+// function getRangeReport(start, end) {
+//   var obj = {};
+
+//   var range = [];
+//   var total = 0;
+//   var odds = [];
+//   var evens = [];
+//   var average = 0;
+//   for (var i = start; i <= end; i++) {
+//     range.push(i);
+//     total += i;
+//     if (i % 2 !== 0) {
+//       odds.push(i);
+//     }
+//     if (i % 2 === 0) {
+//       evens.push(i);
+//     }
+//   }
+//   average = total / range.length;
+
+//   obj.total = total;
+//   obj.odds = odds;
+//   obj.evens = evens;
+//   obj.range = range;
+//   obj.average = average;
+
+//   return obj;
+
+// }
