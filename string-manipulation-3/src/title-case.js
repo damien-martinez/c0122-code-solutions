9929987159e89eb830db39dc50a5e32646a4ab5c
Damien Martinez <mart.damien1@gmail.com>
2022-03-02 10:11:33 -0800

    still working on string-manipulation-3

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,36 +1,13 @@
 /* exported titleCase */
 
-function titleCase(title) {
-  title = title.toLowerCase() + ' ';
-  var subString = '';
-  var spaceCount = 0;
-  var lastChar = '';
-  var newTitle = '';
+// function titleCase(title) {
+//   title = title.toLowerCase();
+//   var minorWords = ['or', 'nor', 'but', 'the', 'a', 'an', 'the', 'like', 'as', 'at', 'by', 'for', 'in', 'of', 'on', 'per', 'to'];
 
-  // debugger;
+//   var subString = '';
+//   var capSubString = '';
+//   var newTitle = '';
+//   var spaceCounter = 0;
+//   var title = false;
 
-  for (var i = 0; i < title.length; i++) {
-    if (title[i] !== ' ') {
-      subString += title[i];
-    } else {
-      spaceCount++;
-
-      if (subString === 'javascript') {
-        subString = 'JavaScript';
-      } else if (subString === 'api') {
-        subString = 'API';
-      } else if (subString.length > 3 || spaceCount === 1 || lastChar === ':') {
-        if (subString === 'javascript:') {
-          subString = 'JavaScript:';
-        }
-        var upperCase = subString[0].toUpperCase();
-        subString = upperCase + subString.slice(1, subString.length);
-      }
-      lastChar = subString[subString.length - 1];
-      newTitle += subString + ' ';
-      subString = '';
-
-    }
-  }
-  return newTitle.slice(0, newTitle.length - 1);
-}
+// }
