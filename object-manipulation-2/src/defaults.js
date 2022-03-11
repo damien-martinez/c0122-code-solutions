470a9d8d9547feea83e6d037f70a5e84ba28e8b9
damien-martinez <mart.damien1@gmail.com>
2022-03-11 23:49:35 +0000

    Removed some conditionals that would never trigger.

--- object-manipulation-2/src/defaults.js
+++ object-manipulation-2/src/defaults.js
@@ -3,14 +3,7 @@
 // a function that takes in two arguments that are both objects and returns an object that has the properties of the first object and properties of the second object if they are not already in the first object
 
 // create a variable and assign zero int to it
-// check if first argument is an empty object
-// if so, iterate through second object using for in  loop
-// in for loop, assign the value of source object at given property  to target at property
 
-// if target not equal to empty object , check if source is an empty object
-// if so, return target
-
-// else,
 // run a for in loop that iterates through source
 // within code block, run a for in loop that iterates through target
 // within code block, check if property of source is equal to property of target
@@ -25,27 +18,20 @@
 
 function defaults(target, source) {
   var checker = 0;
-  if (target === {}) {
-    for (var prop in source) {
+
+  for (var prop in source) {
+    for (var targProp in target) {
+      if (prop === targProp) {
+        checker++;
+
+      }
+    }
+
+    if (checker === 0) {
       target[prop] = source[prop];
     }
-  } else if (source === {}) {
-    return target;
-  } else {
-    for (prop in source) {
-      for (var targProp in target) {
-        if (prop === targProp) {
-          checker++;
-
-        }
-      }
-
-      if (checker === 0) {
-        target[prop] = source[prop];
-      }
-      checker = 0;
-    }
-
+    checker = 0;
   }
+
   return target;
 }
