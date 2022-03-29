2e97cdcc57f475a5c5c464f303168af9370f4708
damien-martinez <mart.damien1@gmail.com>
2022-03-29 06:48:10 +0000

    Completed react-elements

---
+++ react-elements/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const $root = document.querySelector('#root');
+
+const $h1 = React.createElement('h1', null, 'Hello React');
+
+ReactDOM.render($h1, $root);
